519139394e3dda2dc889aaf01d65c607f576af92
unknown <micposso@gmail.com>
2020-05-09 20:11:35 -0400

    first commit

---
+++ config/init.php
@@ -0,0 +1,14 @@
+<?php
+// Confi file
+require_once 'config.php';
+
+
+// Autoloader
+function my_autoloader($class_name){
+  require_once 'lib/'.$class_name. '.php';
+}
+
+spl_autoload_register('my_autoloader');
+
+echo 'test';
+
