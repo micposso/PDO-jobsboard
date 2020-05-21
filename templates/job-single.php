9ee63306e627bd9985a8cd9f24eb812a4ede88d7
unknown <micposso@gmail.com>
2020-05-20 22:34:34 -0400

    add single job functions and template

--- templates/job-single.php
+++ templates/job-single.php
@@ -1,1 +1,10 @@
-<?php echo $job->job_title;  ?>
+<?php include '../includes/header.php'; ?>
+  <div class="jumbotron">
+    <h1><?php echo $job->job_title; ?></h1>
+      <p class="lead">
+      <?php echo $job->description; ?></p>
+      <small><?php echo $job->contact_user; ?></small>
+      <small><?php echo $job->post_date; ?></small>
+      <p><a href="" class="btn btn-lg btn-success">Apply</a></p>
+  </div>
+<?php include '../includes/footer.php'; ?>
